f883ca529dad63b27624dfcd1ffa70b69432bdac
Sean Sorrell <seansorrell@gmail.com>
2011-01-22 20:02:37 -0500

    library done for now - time for the interface

--- lib/gojira.rb
+++ lib/gojira.rb
@@ -1,30 +1,65 @@
 require 'jira4r'
 class Gojira
-  attr_reader :user
-  def initialize(url)
-    @jira = Jira4R::JiraTool.new 2, url
-    @jira.logger=Logger.new("/dev/null")
-  end
+	attr_reader :user
+	def initialize(url)
+		@jira = Jira4R::JiraTool.new 2, url
+		@jira.logger=Logger.new("/dev/null")
+	end
 
-  def login(user,pass)
-    @user = user
-    @jira.login user, pass
-  end
+	def login(user,pass)
+		@user = user
+		@jira.login user, pass
+	end
 
-  def projects
-    @jira.getProjectsNoSchemes
-  end
+	def projects
+		@jira.getProjectsNoSchemes
+	end
 
-  def user_issues(limit = 10)
-    @jira.getIssuesFromJqlSearch "assignee = currentUser()", limit
-  end
+	def user_issues(limit = 10)
+		@jira.getIssuesFromJqlSearch "assignee = currentUser()", limit
+	end
 
-  def issue(key)
-    @jira.getIssue(key)
-  end
+	def issue(key)
+		@jira.getIssue(key)
+	end
 
-  def method_missing(sym, *args, &block)
-    @jira.send sym, *args, &block
-  end
+	def statuses
+		@statuses ||= @jira.getStatuses.map do |s| 
+			{
+				:id => s.id, 
+				:name => s.name,
+				:desc => s.description,
+			}
+		end
+	end
 
+	def status id
+		statuses.find{|s| s[:id] == id}
+	end
+
+	def issue_status(key)
+		issue = issue(key)
+		status(issue.status)
+	end
+	
+	def valid_actions(key)
+		statuses.select do |s| 
+			@jira.getAvailableActions(key).map(&:id).include? s[:id]
+		end
+	end
+
+	def add_comment(key, body)
+		comment = Jira4R::V2::RemoteComment.new
+		comment.body = body
+		@jira.addComment key, comment rescue return nil
+		comment
+	end
+
+	def set_status(issue_key, status_key)
+		@jira.progressWorkflowAction("FWL-696", 1, [])
+	end
+
+	def method_missing(sym, *args, &block)
+		@jira.send sym, *args, &block
+	end
 end
--- spec/gojira_spec.rb
+++ spec/gojira_spec.rb
@@ -1,37 +1,66 @@
 require File.join(File.expand_path(File.dirname(__FILE__)), 'spec_helper')
 require 'gojira'
 
+describe "gojira" do
+	before :all do
+		@gojira = Gojira.new "http://sandbox.onjira.com"
+	end
 
-describe "connect" do
-  before :all do
-    @gojira = Gojira.new "http://sandbox.onjira.com"
-  end
+	it "should connect to the API" do
+		@gojira.should_not be_nil
+	end
 
-  it "should connect to the API" do
-    @gojira.should_not be_nil
-  end
-  
-  it "should login to the API" do
-     @gojira.login("gojira", "gojira").should_not be_nil
-  end
+	it "should login to the API" do
+		@gojira.login("gojira", "gojira").should_not be_nil
+	end
 
-  describe "logged in actions" do
-    before :all do
-      @gojira.login("gojira", "gojira")
-    end
+	describe "logged in actions" do
+		before :all do
+			@gojira.login("gojira", "gojira")
+		end
 
-    it "should list all projects" do
-      @gojira.projects.should_not be_nil
-    end
+		it "should list all projects" do
+			@gojira.projects.should_not be_nil
+		end
 
-    it "should list all issues assigned to the current user" do
-      @gojira.user_issues.should_not be_nil
-      @gojira.user_issues.map(&:assignee).uniq.should == [@gojira.user]
-    end
+		it "should list all issues assigned to the current user" do
+			@gojira.user_issues.should_not be_nil
+			@gojira.user_issues.map(&:assignee).uniq.should == [@gojira.user]
+		end
 
-    it "should return a particular issue" do
-      issues = @gojira.user_issues
-      @gojira.issue(issues.first.key).should_not be_nil
-    end
-  end
+		it "should return a particular issue" do
+			issues = @gojira.user_issues
+			@gojira.issue(issues.first.key).should_not be_nil
+		end
+
+		it "should return an issue's status" do
+			issue_key = @gojira.user_issues.first.key
+			status = @gojira.issue_status(issue_key)
+			status.should_not be_nil
+			["Open", "In Progress", "Reopened", "Resolved", "Closed", "QA", "Approved", "Develop"].should include status[:name]
+		end
+
+		it "should return a particular status" do
+			statuses = @gojira.statuses
+			@gojira.status(statuses.first[:id]).should == statuses.first
+		end
+
+		it "should return available statuses for an issue" do
+			issue_key = @gojira.user_issues.first.key
+			@gojira.valid_actions(issue_key).should_not be_nil
+		end
+
+		it "should set an issue's status" do
+			issue_key = @gojira.user_issues.first.key
+			status_key = @gojira.valid_actions(issue_key).first
+			@gojira.set_status(issue_key, status_key).should == @gojira.status(status_key)
+			status_key = @gojira.valid_actions(issue_key).first
+			@gojira.set_status(issue_key, status_key).should == @gojira.status(status_key)
+		end
+
+		it "should add a comment to an issue" do
+			issue_key = @gojira.user_issues.first.key
+			@gojira.add_comment(issue_key, "my comment").body.should == "my comment"
+		end
+	end
 end
